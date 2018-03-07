c485a034c6ba51acf6f64e00b189cbab7a01be22
Wojtek Okoński <wojteko22.96@gmail.com>
2018-03-07 11:30:24 +0100

    Fix individual to be consistent

--- src/main/kotlin/Individual.kt
+++ src/main/kotlin/Individual.kt
@@ -1,16 +1,12 @@
 class Individual(size: Int) {
-    private val genes = List(size) { it }.shuffled()
-
-    fun print() {
-        println(genes)
-    }
+    private val genes = List(size) { it + 1 }.shuffled()
 
     val cost = run {
         var result = 0
         for (rowIndex in 0 until size) {
             for (columnIndex in 0 until size) {
                 result += inputData.flowMatrix[rowIndex][columnIndex] *
-                        inputData.distanceMatrix[genes[rowIndex]][genes[columnIndex]]
+                        inputData.distanceMatrix[genes[rowIndex] - 1][genes[columnIndex] - 1]
             }
         }
         result
