adf6696005129add8a9ed2cee8c74c475762d164
Wojtek Okoński <wojteko22.96@gmail.com>
2018-03-07 11:08:59 +0100

    Add individual

---
+++ src/main/kotlin/Individual.kt
@@ -0,0 +1,18 @@
+class Individual(size: Int) {
+    private val genes = List(size) { it }.shuffled()
+
+    fun print() {
+        println(genes)
+    }
+
+    val cost = run {
+        var result = 0
+        for (rowIndex in 0..size) {
+            for (columnIndex in 0..size) {
+                result += inputData.flowMatrix[rowIndex][columnIndex] *
+                        inputData.distanceMatrix[genes[rowIndex]][genes[columnIndex]]
+            }
+        }
+        result
+    }
+}
---
+++ src/main/kotlin/InputData.kt
@@ -0,0 +1,25 @@
+import java.io.File
+
+val inputData = InputData.readFrom("src/main/resources/had12.dat")
+
+typealias Matrix = List<List<Int>>
+
+class InputData(val size: Int, val flowMatrix: Matrix, val distanceMatrix: Matrix) {
+
+    companion object {
+        fun readFrom(pathname: String): InputData {
+            val file = File(pathname)
+            val content = file.readText().trim().split(Regex("\\n\\s*\\n"))
+            val size = content[0].toInt()
+            val flowMatrix = content[1].toMatrix()
+            val distanceMatrix = content[2].toMatrix()
+            return InputData(size, flowMatrix, distanceMatrix)
+        }
+
+        private fun String.toMatrix() = toMatrixRows().map {
+            it.split(Regex("\\s\\s")).map { it.toInt() }
+        }
+
+        private fun String.toMatrixRows() = split(Regex("\\n")).map { it.trim() }
+    }
+}
