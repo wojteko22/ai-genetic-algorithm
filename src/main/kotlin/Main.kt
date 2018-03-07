dbd9190e0cc1ee85ce6b3176c595322caf0ccc5e
Wojtek Okoński <wojteko22.96@gmail.com>
2018-03-07 16:46:57 +0100

    Implement breeding

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,4 +2,6 @@ fun main(args: Array<String>) {
     val inputData = InputData.readFrom("src/main/resources/had12.dat")
     val population = Population(100, inputData)
     println(population.bestCost)
+    val newPopulation = population.breed(5)
+    println(newPopulation.bestCost)
 }
--- src/main/kotlin/Population.kt
+++ src/main/kotlin/Population.kt
@@ -1,7 +1,32 @@
-class Population(size: Int, inputData: InputData) {
-    private val individuals = Array(size) { Individual(inputData) }
+import java.util.*
+import kotlin.NoSuchElementException
+
+class Population(private val individuals: List<Individual>) {
+
+    private val size = individuals.size
+
+    constructor(size: Int, inputData: InputData) : this(List(size) { Individual(inputData) })
 
     val bestCost = individuals
         .map { it.cost }
         .min() ?: throw NoSuchElementException("Empty population")
+
+    fun breed(tournamentSize: Int): Population {
+        val children = (1..size).map {
+            val selected = selectInTournament(tournamentSize)
+            selected.outcross(randomIndividual)
+        }
+        return Population(children)
+    }
+
+    private fun selectInTournament(tournamentSize: Int): Individual {
+        val selectedGroup = individuals.shuffled().take(tournamentSize)
+        return selectedGroup.maxBy { it.cost } ?: throw NoSuchElementException("No individuals in tournament")
+    }
+
+    private val randomIndividual: Individual
+        get() {
+            val nextInt = Random().nextInt(size)
+            return individuals[nextInt]
+        }
 }
