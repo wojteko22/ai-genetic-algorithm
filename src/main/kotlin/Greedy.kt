b18522871a032c49da0fd714fe9a05de732ce9ee
Wojtek Okoński <wojteko22.96@gmail.com>
2018-03-14 12:08:33 +0100

    Refactor greedy

--- src/main/kotlin/Greedy.kt
+++ src/main/kotlin/Greedy.kt
@@ -1,3 +1,7 @@
+fun main(args: Array<String>) {
+    GreedyQapFinder(20).print()
+}
+
 class GreedyQapFinder(dataNumber: Int) {
     private val facilitiesData = FacilitiesData.readFrom(dataNumber)
     private val numberOfFacilitiesData = facilitiesData.numberOfFacilities
@@ -17,34 +21,27 @@ class GreedyQapFinder(dataNumber: Int) {
         return currentResult
     }
 
-    private fun findTwoFirstLocations(): List<Location> {
-        val pairs = allLocations.flatMap { firstItem ->
-            (allLocations - firstItem).map { secondItem ->
-                listOf(firstItem, secondItem)
-            }
+    private fun findTwoFirstLocations(): List<Location> = allPossiblePairs(allLocations)
+        .map { it.toList() }
+        .minBy { calculateCost(it) } ?: error("Not enough locations")
+
+
+    private fun allPossiblePairs(list: Iterable<Int>): List<Pair<Int, Int>> = list.flatMap { firstItem ->
+        (list - firstItem).map { secondItem ->
+            Pair(firstItem, secondItem)
         }
-        return pairs.minBy { calculateCost(it) } ?: error("Not enough locations")
     }
+
     private fun findBestNextLocation(currentResult: List<Location>): List<Location> {
         val availableLocations = allLocations - currentResult
-        return availableLocations.map { currentResult + it }.minBy { calculateCost(it) } ?: error("No available Location")
+        return availableLocations
+            .map { currentResult + it }
+            .minBy { calculateCost(it) } ?: error("No available Location")
     }
 
-    private fun calculateCost(locations: List<Location>): Int {
-        var cost = 0
-        for (firstFacility in 0 until locations.size) {
-            for (secondFacility in (firstFacility + 1) until locations.size) {
-                cost += facilitiesData.flowMatrix[firstFacility][secondFacility] *
-                        facilitiesData.distanceMatrix[locations[firstFacility] - 1][locations[secondFacility] - 1] +
-                        facilitiesData.flowMatrix[secondFacility][firstFacility] *
-                        facilitiesData.distanceMatrix[locations[secondFacility] - 1][locations[firstFacility] - 1]
-            }
+    private fun calculateCost(locations: List<Location>): Int = allPossiblePairs(locations.indices)
+        .sumBy { (firstFacility, secondFacility) ->
+            facilitiesData.flowMatrix[firstFacility][secondFacility] *
+                    facilitiesData.distanceMatrix[locations[firstFacility] - 1][locations[secondFacility] - 1]
         }
-        return cost
-    }
 }
-
-fun main(args: Array<String>) {
-    GreedyQapFinder(12).print()
-}
-
